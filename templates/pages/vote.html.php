d9c462f6b67f1b2ac22b2ecdbe13a2fd5bd6df45
PlayDargoon <VongolaIvanrogue@gmail.com>
2025-09-27 18:59:27 +1100

    Добавил информацию в страницу голосование

--- templates/pages/vote.html.php
+++ templates/pages/vote.html.php
@@ -2,23 +2,75 @@
 // templates/pages/vote.html.php
 ?>
 <div class="body">
-    <h2>Голосование за сервер Azeroth</h2>
-    <p>
-        Поддержите наш сервер, проголосовав за нас на MMOTOP!<br>
-        Ваш голос помогает нам подниматься выше в рейтинге, привлекать новых игроков и развивать проект.
-    </p>
-    <div style="margin: 20px 0; text-align:center;">
-        <a href="https://wow.mmotop.ru/servers/36327/votes/new" target="_blank" class="headerButton" style="font-size:18px; padding:10px 24px; background:gold; color:#222; border-radius:8px; text-decoration:none; font-weight:bold;">Голосовать на MMOTOP</a>
+    <h2>Голосование</h2>
+
+    <div class="pt" style="text-align:center">
+        <img src="/images/lavka_redkostey_blue_310.jpg" width="310" height="103" alt="?">
     </div>
-    <h3>Условия получения награды:</h3>
+
+<br>
+<div class="small pt">
+<img src="/images/icons/menialo.png" alt="*" style="float:left;margin-right:8px;" class="ic32" width="32" height="32">
+    Приветствую тебя, герой! У Вас появилась возможность получать бонусы в игре, путем голосования за Azeroth на сайтах с рейтингами сервера. 
+Я расскажу как проголосовать и условие получения награды. На данный момент для голосования подключен  MMOTOP.
+</div>
+<br>
+
+<div style="display: flex; justify-content: center; align-items: center;">
+    <a class="headerButtonMy" href="https://wow.mmotop.ru/servers/36327/votes/new" style="text-align:center; display:inline-block;">
+        Я голосую за AZEROTH!
+    </a>
+</div>
+<br>
+
+
+
+   <h2 class="pb10 _font-art font16">Как голосовать?</h2>
+
+    <ul class="bluepost">
+            <li>    <img src="/images/icons/arr1.png"> 
+                Авторизируетесь на <a href="https://wow.mmotop.ru/servers/36327/votes/new">сайте через ВК/ФБ</a>, затем подтверждаете учетную запись на почте или 
+                создаете учетную запись на сайте и следуете инструкциям, но проще и быстрее испрользовать соц.сети.
+                 </li>
+             <li>   <img src="/images/icons/arr1.png"> 
+                Далее после регистрации/авторизации сдвигаете ползунок.
+                </li>
+             <li>    <img src="/images/icons/arr1.png"> 
+                 После вводите <span class="yellow">ЛОГИН</span> от аккаунта и жмете проголосовать.
+
+                 <p> <div class="pt minor small">
+                <img src="/images/icons/attention_gold.png" alt="!" width="12" height="12"> 
+                 **ник персонажа вводить смысла не имеет, только <span class="yellow">ЛОГИН</span>
+                </div>
+               </li>
+             <li>   <img src="/images/icons/arr1.png"> 
+                Затем просто ждете когда прийдет уведовлени, что голос засчитан.
+                </li>
+             <li>   <img src="/images/icons/arr1.png"> 
+                Если по какой-то причине сайт не доступен, пробуйте голосовать через VPN.
+                </li>
+    </ul>
+        
+<br>
+
+            <h2 class="pb10 _font-art font16">Как начисляются?</h2>
+    
+       
+    
+    
     <ul>
-        <li>Голосовать можно не чаще одного раза в 16 часов.</li>
-        <li>После голосования на MMOTOP вы автоматически получите 1 монету на сайте (валюта за голосование).</li>
-        <li>Монеты можно использовать для бонусов и внутриигровых привилегий (раздел будет добавлен позже).</li>
-        <li>Голос засчитывается только если вы авторизованы на MMOTOP и выбрали своего персонажа/аккаунт.</li>
-        <li>Если награда не пришла сразу — подождите 5-10 минут и обновите страницу.</li>
+        <li> <img src="/images/icons/arr1.png"> Голосовать можно не чаще одного раза в 16 часов.</li>
+        <li><img src="/images/icons/arr1.png"> За 1 голос начисляется 1 бонус.</li>
+        <li><img src="/images/icons/arr1.png"> Голоса можно использовать для покупки внутриигровых привилегий<span class="minor">(Раздел в разработке)</span>.</li>
+        <li><img src="/images/icons/arr1.png"> Голос засчитывается только если вы авторизованы на <span class="yellow">MMOTOP</span> и указали <span class="yellow">ЛОГИН</span> от своего.</li>
+        <li><img src="/images/icons/arr1.png"> Бонусы за голос начисляются в течении часа.</li>
+        <li><img src="/images/icons/arr1.png"> Если по какойто причине бонусы за голоса так и не зачислились, обратитесь в поддержку.</li>
     </ul>
     <p>
         <b>Спасибо за вашу поддержку! Каждый голос важен для развития сервера.</b>
     </p>
+
+    
+
+
 </div>
